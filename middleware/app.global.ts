93f0192231739d95ba3ad9783b1e9a590788a632
Faisal Achmad Dwi Cahyono <faisalachmaddwic@gmail.com>
2023-12-14 00:44:50 +0700

    app cookie in tab

--- middleware/app.global.ts
+++ middleware/app.global.ts
@@ -1,9 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const name = to.name?.toString();
-    const cookie = useCookie("tab");
-    cookie.value = JSON.stringify({
-        path: to.path,
-        name: name!.charAt(0).toUpperCase() + name?.slice(1),
-        value: to.name,
-    });
+    if (to.path !== "/login" && to.path !== "/forget-password") {
+        const name = to.name?.toString();
+        const cookie = useCookie("tab");
+        cookie.value = JSON.stringify({
+            path: to.path,
+            name: name!.charAt(0).toUpperCase() + name?.slice(1),
+            value: to.name,
+        });
+    }
 });
